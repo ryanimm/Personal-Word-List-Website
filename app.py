9b4eb69198e1ab66e4afd55c718810b4fd5a926e
madunbulan <85464475+madunbulan@users.noreply.github.com>
2023-05-08 22:11:30 +0700

    Website Personal Word List

---
+++ app.py
@@ -0,0 +1,150 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from pymongo import MongoClient
+import requests
+from datetime import datetime
+from bson import ObjectId
+
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+app = Flask(__name__)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+
+
+@app.route('/')
+def main():
+    words_result = db.words.find({}, {'_id': False})
+    words = []
+    
+    for word in words_result:
+        definition = word['definitions'][0]['shortdef']
+        definition = definition if type(definition) is str else definition[0]
+        words.append({
+            'word': word['word'],
+            'definition': definition,
+        })
+    return render_template(
+        'index.html',
+        words=words
+    )
+
+@app.route('/error')
+def error():
+    word = request.args.get('word')
+    suggestions = request.args.get('suggestions')
+    if suggestions:
+        suggestions = suggestions.split(',')
+    return render_template(
+        'error.html',
+        word=word,
+        suggestions=suggestions
+    )
+
+
+@app.route('/detail/<keyword>')
+def detail(keyword):
+    api_key = 'd329b7ac-6242-4c5c-b5dd-24c5ea9652a8'
+    url = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{keyword}?key={api_key}'
+    response = requests.get(url)
+    definitions = response.json()
+
+    if not definitions:
+        return redirect(url_for(
+            'error',
+            word=keyword
+        ))
+
+    if type(definitions[0]) is str:
+        return redirect(url_for(
+            'error',
+            word=keyword,
+            suggestions=','.join(definitions)
+        ))
+
+    status = request.args.get('status_give', 'new')
+    return render_template(
+        'detail.html',
+        word=keyword,
+        definitions=definitions,
+        status=status
+    )
+
+@app.route('/api/save_word', methods=['POST'])
+def save_word():
+    json_data = request.get_json()
+    word = json_data.get('word_give')
+    definitions = json_data.get('definitions_give')
+    doc = {
+        'word': word,
+        'definitions': definitions,
+        'date': datetime.now().strftime('%Y%m%d'),
+    }
+    db.words.insert_one(doc)
+    return jsonify({
+        'result': 'success',
+        'msg': f'the word, {word}, was saved!!!',
+    })
+
+
+@app.route('/api/delete_word', methods=['POST'])
+def delete_word():
+    word = request.form.get('word_give')
+    db.words.delete_one({'word': word})
+    db.examples.delete_many({"word" : word})
+    return jsonify({
+        'result': 'success',
+        'msg': f'the word {word} was deleted'
+    })
+
+
+@app.route('/api/get_exs', methods=['GET'])
+def get_exs(): 
+    word = request.args.get('word')
+    data = db.examples.find({'word': word})
+    examples = []
+    for example in data:
+        examples.append({
+            'example': example.get('example'),
+            'id': str(example.get('_id')),
+        })
+    return jsonify({'result': 'success', 'examples' : examples})
+
+
+@app.route('/api/save_ex', methods=['POST'])
+def save_ex():
+    example = request.form.get('example')
+    word = request.form.get('word')
+
+    doc = {
+        'word': word,
+        'example' : example,
+    }
+    db.examples.insert_one(doc)
+
+    return jsonify({'result': 'success',
+                    'msg' : f'your example, {example}, for the word, {word} was saved!'})
+
+
+@app.route('/api/delete_ex', methods=['POST'])
+def delete_ex():
+    id = request.form.get('id')
+    word = request.form.get('word')
+    db.examples.delete_one({'_id': ObjectId(id)})
+    return jsonify({'result': 'success', 
+                    'msg':f'Your word, {word}, was deleted!'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
